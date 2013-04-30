d0cc941c787bf32155dfedf510559d1fb2d63911
scottwerner <scottwerner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-30 04:57:18 +0000

    typos. stupid typos and late night editing
    git-svn-id: https://plugins.svn.wordpress.org/no-image-link/tags/1.2@705765 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- no-imagelink.php
+++ no-imagelink.php
@@ -4,7 +4,7 @@ Plugin Name: No Image Link
 Plugin URI: http://scottwernerdesign.com/plugins/no-image-link/
 Description: Defaults the Image Link option when adding a photo to 'none'.
 Version: 1.2
-Author: scottwerner
+Author: Scott Werner
 Author URI: http://scottwernerdesign.com
 License: GPL
 */
